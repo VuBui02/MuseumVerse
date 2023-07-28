3fe024ed64e0485bbb02d9d9347328ced8b08735
NghiaCH <nghiach@rikkeisoft.com>
2023-07-28 17:42:57 +0700

    feat: create NFT

---
+++ src/components/screens/CreateNFT/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { CreateForm } from "./components/CreateForm";
+
+export const CreateNFTScreen = () => {
+  return (
+    <div className="mt-12 md:mt-0 md:py-24 md:px-12 lg:px-16 xl:px-28">
+      <p>Create NFT</p>
+      <CreateForm />
+    </div>
+  );
+};
